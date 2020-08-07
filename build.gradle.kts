78515f27231bef55ddcc7692f5fa8ac22568ff84
Clement Stoquart <clement.stoquart@ekino.com>
2020-08-07 10:20:06 +0200

    chore: upgrade tests deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ dependencies {
   implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.10.0")
 
   testImplementation(gradleTestKit())
-  testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
-  testImplementation("org.assertj:assertj-core:3.15.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+  testImplementation("org.assertj:assertj-core:3.16.1")
 }
 
 tasks {
